006b3a99eafc63a6a28eec3a40fe7c22566809a9
neooxdev <pfirame18@gmail.com>
2026-04-21 19:28:18 +0530

    automation script added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://prasadf18.github.io',
+  base: '/prasadf18.github.io',
+});
